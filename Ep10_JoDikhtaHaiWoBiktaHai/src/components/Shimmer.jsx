18995dbdca4c568e4a98599f7394a1814a86adb0
anujjha13 <anujjha1301@gmail.com>
2024-12-21 18:06:01 +0530

    Adding Shimmer css

--- Ep10_JoDikhtaHaiWoBiktaHai/src/components/Shimmer.jsx
+++ Ep10_JoDikhtaHaiWoBiktaHai/src/components/Shimmer.jsx
@@ -1,14 +1,22 @@
 const Shimmer = () =>{
     return (
-        <div className="shimmer-container">
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
+        <div className="flex flex-wrap justify-around mt-10">
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
+            <div className="bg-red-400 size-96 m-2 animate-pulse rounded-md"></div>
         </div>
     )
 };
